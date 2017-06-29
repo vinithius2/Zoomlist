47ea9406825c6ff61ee165269efcfd9571b86b66
vinithius2 <marcos.filho@gmail.com>
2017-06-29 16:36:52 -0300

    Prosseguindo com melhorias

---
+++ app/src/main/java/br/com/calculafeira/calculafeira/DialogFragment/DialogFragmentCreate.java
@@ -0,0 +1,10 @@
+package br.com.calculafeira.calculafeira.DialogFragment;
+
+import android.app.DialogFragment;
+
+/**
+ * Created by DPGE on 29/06/2017.
+ */
+
+public class DialogFragmentCreate extends DialogFragment {
+}
